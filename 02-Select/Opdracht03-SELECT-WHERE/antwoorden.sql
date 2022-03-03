5cfa3a534fa3fe408998f1752f89dd10a8288efb
antonio <voetballergamerajdo@gmail.com>
2022-03-03 13:20:58 +0100

    klaar

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank=7
 -- Opdracht 2 
-
+SELECT * FROM jaar2016 WHERE country="nederland"
 -- Opdracht 3
-
+select country="nederland", score FROM jaar2015
 -- Opdracht 4
-
+select country="nederland", score FROM jaar2016
 -- Opdracht 5
-
+SELECT * FROM jaar2016 WHERE region="noord-amerika"
 -- Opdracht 6 
-
+SELECT * FROM jaar2016 WHERE rank=25
 -- Opdracht 7 
-
+SELECT * FROM jaar2016 WHERE score="7087"
 -- Opdracht 8
-
+SELECT * FROM jaar2016 WHERE score="5"
 -- Opdracht 9
-
+SELECT * FROM jaar2016 WHERE country="ierland"
 -- Opdracht 10
+SELECT * FROM jaar2016 WHERE region="Latin America and Caribbean"
