12b7d24d69f5909bd1c37cc1c8d35b9f962811f3
antonio <voetballergamerajdo@gmail.com>
2022-03-04 11:49:51 +0100

    klaar

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-6
+DELETE FROM player WHERE name = "david silva";
 -- Opdracht 2 
-
+DELETE from `players` WHERE club="willem 2"
 -- Opdracht 3
-
+DELETE FROM player WHERE nationality = "belgium" AND club="barcelona";
 -- Opdracht 4
-
+DELETE FROM player WHERE age>="38";
 -- Opdracht 5
+DELETE FROM player WHERE id="169595"
