54f2c10f4c492969344b6c09156b1bda1a3c53d1
antonio <voetballergamerajdo@gmail.com>
2022-03-08 13:03:25 +0100

    klaar

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,9 +1,9 @@
 -- Opdracht 1 
 SELECT * FROM players WHERE nationality = "Spain" AND club ="chelsea";
 -- Opdracht 2 
-SELECT * FROM players WHERE nationality = "Spain" AND wage = 15000      AND   age="17";
+SELECT * FROM players WHERE nationality = "Spain" AND wage = 15000      AND   age=17;
 -- Opdracht 3
-SELECT * FROM players WHERE club = "liverpool"  AND   age>="20";
+SELECT * FROM players WHERE club = "liverpool"  AND   age>=20;
 -- Opdracht 4
 SELECT * FROM players WHERE nationality = "nederland" AND club ="ajax";
 -- Opdracht 5
@@ -16,16 +16,16 @@ SELECT  name,age,club FROM players WHERE club="az alkmaar";
 SELECT  name,wage FROM players WHERE club="manchester city" AND nationality="brazilian";
 
 -- Opdracht 9
-SELECT * FROM players WHERE wage = 10000 AND   age="30";
+SELECT * FROM players WHERE wage = 10000 AND   age=30;
 -- Opdracht 10
-SELECT  name,age FROM players WHERE nationality="spain";
+SELECT  name,age FROM players WHERE nationality="spain" OR nationality="portugal";
 -- Opdracht 11
 SELECT  name,age,club FROM players WHERE club="chelsea";
 -- Opdracht 12
-SELECT name,club FROM players WHERE wage="10000"  AND   age>="40";
+SELECT name,club FROM players WHERE wage=10000  AND   age>=40;
 -- Opdracht 13
 SELECT * FROM players WHERE club = "ajax"  AND   nationality="netherlands";
 -- Opdracht 14
-SELECT * FROM players WHERE nationality="england"  AND   age>="20" AND wage>="100000";
+SELECT * FROM players WHERE nationality="england"  AND   age>=20 AND wage>="100000";
 -- Opdracht 15
-SELECT name,age,nationality FROM players WHERE nationality="brazil"  AND   age>="25";
+SELECT name,age,nationality FROM players WHERE nationality="brazil"  AND   age>=25;
